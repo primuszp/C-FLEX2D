f8ff3535dc977319f7f8db2617cc54fbbe3da5c0
symphonylyh <31998628+symphonylyh@users.noreply.github.com>
2019-11-27 23:29:02 -0600

    Windows setting

--- compile.sh
+++ compile.sh
@@ -5,6 +5,14 @@
 # rm build/CMakeCache.txt # if the directory is moved or reconstructed, reset the cmake cache before make
 cd ./build
 cmake ..
-make
 
-# executable will be under ./build/FEM/main
+if [[ "$OSTYPE" == "msys" ]]; then
+    # for Windows system
+    cmake --build .
+    # Note: we need to change the compile flags in /C-FLEX2D/FEM/CMakeLists.txt
+    # executable will be under ./buld/FEM/Debug/main.ext
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # for Mac OS
+    make
+    # executable will be under ./build/FEM/main
+fi
