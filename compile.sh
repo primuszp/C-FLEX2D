9682f2194df1c8759f8ed2a7582761033cce3ff3
symphonylyh <31998628+symphonylyh@users.noreply.github.com>
2019-11-28 18:49:19 -0600

    windows compile guide

--- compile.sh
+++ compile.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # bash script will run as in a sandbox, so cd here won't change the pwd outside
 # remember to `chmod +x compile.sh` for this file or use `sh compile.sh`
+
+# 1. setup build directory
 [[ -d build ]] || mkdir build
-# rm build/CMakeCache.txt # if the directory is moved or reconstructed, reset the cmake cache before make
+
+# rm build/CMakeCache.txt # if the directory is moved or reconstructed, reset the cmake cache before make. When you see error, just remove this cache file
+
+# 2. generate Makefile
 cd ./build
 cmake ..
 
-if [[ "$OSTYPE" == "msys" ]]; then
-    # for Windows system
-    cmake --build .
-    # Note: we need to change the compile flags in /C-FLEX2D/FEM/CMakeLists.txt
-    # executable will be under ./buld/FEM/Debug/main.ext
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # for Mac OS
-    make
-    # executable will be under ./build/FEM/main
-fi
+# 3. make executable
+make
+# executable will be under ./build/FEM/main
